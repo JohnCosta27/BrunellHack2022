ebcd34574349ff854db8740d4a09c6ad2375ea21
James <jamesarnott2@gmail.com>
2022-11-19 15:09:52 +0000

    feat: added stuff

--- public/src/format/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = () => (
-  <header className="h-16 bg-gradient-to-r from-primary-400 to-primary-600 top-0 w-full text-4xl justify-right flex-col space-x-5 text-white z-10">
-    <div className="flex-1 inline-block align-middle">
-      DeadDropr
-    </div>
-  </header>
-);
-
-export default Header;
