e110434cb9170af0e0c1fda6a1cd497b981519b7
John Costa <johncosta027@gmail.com>
2022-11-19 13:46:14 +0000

    function to save lat and lon values

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as geofire from 'geofire-common';
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -11,7 +12,28 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-export const createDrop = functions.https.onRequest((request, response) => {
-  db.collection("deadDrops").add({ hello: "World" });
+export const createDrop = functions.https.onRequest(async (request, response) => {
+  const body = request.body;
+  if (!body["lon"] || !body["lat"]) {
+    response.send("bruh, send me long and lat");
+    return;
+  }
+
+  const hash = geofire.geohashForLocation([body["lon"], body["lat"]]);
+  const current = db.collection("deadDrops").doc(hash);
+  const doc = await current.get();
+
+  if (!doc.exists) {
+    current.set({
+      lat: body["lat"],
+      lon: body["lon"],
+      messages: ["Hello World"],
+    });
+  } else {
+    current.update({
+      messages: [...doc.data()!.messages, "Helloooo!"],
+    });
+  }
+
   response.send('idk man');
 });
