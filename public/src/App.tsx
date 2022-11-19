1f4b4e517ab3ee8bda916df4ce4e94009489b3cb
James <jamesarnott2@gmail.com>
2022-11-19 12:40:39 +0000

    chore: committing template/setup files

---
+++ public/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { XyzTransitionGroup } from '@animxyz/react';
+
+import '@/index.css';
+
+const App = () => (
+  <div className="flex justify-center align-middle h-screen">
+      <div className="underline text-5xl bg-white w-full h-20 text-center p-5">DeadDropr</div>
+  </div>
+);
+
+export default App;
