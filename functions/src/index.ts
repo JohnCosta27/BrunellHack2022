05867b8e1a062ebfcb4810cc7f06b7da7235e4ca
John Costa <johncosta027@gmail.com>
2022-11-19 12:58:27 +0000

    functions working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,17 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+const db = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
+
+export const createDrop = functions.https.onRequest((request, response) => {
+  db.collection("deadDrops").add({ hello: "World" });
+  response.send('idk man');
+});
